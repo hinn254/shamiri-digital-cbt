40aff5f3301ad2f5c2a63056cc2d92d77263eb01
Benny <bennyhinnotieno@gmail.com>
2021-02-15 15:32:55 +0300

    added intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,19 +1,9 @@
 import React from "react";
 import "./Intro.css";
+
 import Button from "@material-ui/core/Button";
 
 function Intro({ changeStep = (f) => f, logo }) {
-  // const handleTimeChange = () => {
-  //   console.log(`Executed handle time`);
-  //   let timeValue = 0;
-  //   setInterval((timeValue) => {
-  //     console.log("Time taken");
-  //     timeValue++;
-  //   }, 1000);
-  //   console.log(`The time value is ${timeValue}`);
-  // };
-  // handleTimeChange();
-
   return (
     <div className="introduction">
       <div className="introduction__image">
